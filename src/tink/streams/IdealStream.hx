065547f7b38314304207c23c577b71dcc5d4d502
Kevin Leung <kevinresol@gmail.com>
2017-04-05 14:24:29 +0800

    bring back collect()

--- src/tink/streams/IdealStream.hx
+++ src/tink/streams/IdealStream.hx
@@ -4,7 +4,17 @@ import tink.streams.Stream;
 
 using tink.CoreApi;
 
-typedef IdealStream<Item> = Stream<Item, Noise>;
+@:forward
+abstract IdealStream<Item>(Stream<Item, Noise>) from Stream<Item, Noise> to Stream<Item, Noise> {
+  public function collect():Future<Array<Item>> {
+    var buf = [];
+    return this.forEach(function(x) {
+      buf.push(x);
+      return Resume;
+    }).map(function(c) return buf);
+  }
+}
+
 typedef IdealStreamObject<Item> = StreamObject<Item, Noise>;
 
 class IdealStreamBase<Item> extends StreamBase<Item, Noise> {
